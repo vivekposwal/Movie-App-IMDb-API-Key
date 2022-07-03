536030a0e10b7707566a655674013ef48cd87cd0
vivekposwal <vivekposwal170782@gmail.com>
2022-07-03 14:41:04 +0530

    Added all

---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img src={IMG_API + poster_path} alt={title} />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+    <div className="movie-over">
+      <h2>Overview:</h2>
+      <p>{overview}</p>
+    </div>
+  </div>
+);
+export default Movie;
